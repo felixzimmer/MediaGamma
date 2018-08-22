bcaa2220b95de1cc35c5b5cb0c8214037657ed60
Flex <felixzimmer@gmail.com>
2018-08-22 14:26:57 +0200

    -Added two new classes base structure to help manage the main task

---
+++ EFeatures.scala
@@ -0,0 +1,6 @@
+
+//Enumeration to identify every feature
+object EFeatures extends Enumeration {
+
+  //TODO: work_in_progress
+}
---
+++ FeatureUtils.scala
@@ -0,0 +1,6 @@
+
+//This object provide the feature utility functions
+object FeatureUtils {
+
+  //TODO: work_in_progress
+}
