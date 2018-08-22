693e6798102df51ab01656d2bf347d5844c8ef70
Flex <felixzimmer@gmail.com>
2018-08-22 14:17:18 +0200

    -Completed flattener object

--- InputObject.java
+++ InputObject.java
@@ -10,23 +10,43 @@ public class InputObject {
     @JsonProperty("bid_time")
     private String bidTime;
 
+    //features regarding "app" element
     private List<String> bundleList;
     private List<String> catList;
+
+    //feature regarding the "regs" element
     private List<String> coppaList;
+
+    //features regarding the "site" element
     private List<String> userRatingList;
     private List<String> languageList;
     private List<String> domainList;
     private List<String> idList;
 
-    //TODO: imp is a little bit different, let's leave it here for the moment
-    private List<LinkedHashMap<String, Object>> videoProperty;
-    private List<String> placementList;
-    private List<String> envireomentTypeList;
-    private List<String> bannerHList;
-    private List<String> bannerBattrList;
-    private List<String> bannerWList;
-    private List<String> instlList;
+    //features regarding the "imp" element
+    private List<ImpObject> impObjectList;
 
+    //features regarding the "ext" element
+    private List<String> segmentList;
+    private List<String> targetingCountry;
+    private List<String> targetingRegion;
+    private List<String> targetingMetro;
+    private List<String> targetingCity;
+    private List<String> inventorySourceList;
+    private List<String> timeOfWeekList;
+
+    //features regarding the "device" element
+    private List<String> augomentedOsList;
+    private List<String> augmentedModelList;
+    private List<String> augmentedBrowserList;
+    private List<String> augmentedBrowserVersionList;
+    private List<String> augmentedOsvList;
+    private List<String> augmentedMakeList;
+    private List<String> augmentedDeviceScreenSizeList;
+    private List<String> carrierList;
+    private List<String> deviceTypeList;
+    private List<String> connectionTypeList;
+    private List<String> jsList;
 
 
     public InputObject() {
@@ -57,29 +77,260 @@ public class InputObject {
         this.idList = (List<String>) site.get("id");
     }
 
- /*   @SuppressWarnings("unchecked")
+    @SuppressWarnings("unchecked")
+    @JsonProperty("ext")
+    private void unpackNestExt(Map<String, Object> ext) {
+        this.segmentList = (List<String>) ext.get("segment");
+
+        Map<String, Object> targetingGeoMap = (Map<String, Object>)ext.get("targeting_geo");
+        this.targetingCountry = (List<String>) targetingGeoMap.get("country");
+        this.targetingRegion = (List<String>) targetingGeoMap.get("region");
+        this.targetingMetro = (List<String>) targetingGeoMap.get("metro");
+        this.targetingCity = (List<String>) targetingGeoMap.get("city");
+
+        this.inventorySourceList = (List<String>) ext.get("inventory_source");
+        this.timeOfWeekList = (List<String>) ext.get("time_of_week");
+    }
+
+    @SuppressWarnings("unchecked")
+    @JsonProperty("device")
+    private void unpackNestedDevice(Map<String, Object> device) {
+        Map<String,Object> extMap = (Map<String, Object>) device.get("ext");
+
+        if (extMap != null) {
+            this.augomentedOsList = (List<String>) extMap.get("augomented_os");
+            this.augmentedModelList = (List<String>) extMap.get("augmented_model");
+            this.augmentedBrowserList = (List<String>) extMap.get("augmented_browser");
+            this.augmentedBrowserVersionList = (List<String>) extMap.get("augmented_browser_version");
+            this.augmentedOsvList = (List<String>) extMap.get("augomented_osv");
+            this.augmentedMakeList = (List<String>) extMap.get("augmented_make");
+            this.augmentedDeviceScreenSizeList = (List<String>) extMap.get("augmented_device_screen_size");
+        }
+
+        this.carrierList = (List<String>) device.get("carrier");
+        this.deviceTypeList = (List<String>) device.get("devicetype");
+        this.connectionTypeList = (List<String>) device.get("connectiontype");
+        this.jsList = (List<String>) device.get("js");
+
+    }
+
+    @SuppressWarnings("unchecked")
     @JsonProperty("imp")
     private void unpackNestedImp(List<Map<String, Object>> imp) {
 
         for (Map<String,Object> element : imp) {
 
-            //retrieve video info
             LinkedHashMap<String,Object> videoLinkedHashMap = (LinkedHashMap<String, Object>)element.get("video");
-            if (videoLinkedHashMap != null && videoProperty == null) {
-                this.videoProperty = new ArrayList<>();
+
+
+            List<String> placementList = null;
+            List<String> placementTypeList = null;
+            List<String> enviromentTypeList = null;
+
+            Map<String, Object> extMap = (Map<String,Object>) element.get("ext");
+
+            if (extMap != null) {
+                placementList = (List<String>) extMap.get("placement");
+                placementTypeList = (List<String>) extMap.get("placement_type");
+                enviromentTypeList = (List<String>) extMap.get("environment_type");
             }
-            this.videoProperty.add((LinkedHashMap<String, Object>)element.get("video"));
+
+            List<String> bannerHList = null;
+            List<String> bannerBattrList = null;
+            List<String> bannerWList = null;
+
+            Map<String, Object> bannerMap = (Map<String, Object>) element.get("banner");
+
+            if (bannerMap != null) {
+                bannerHList = (List<String>) bannerMap.get("h");
+                bannerBattrList = (List<String>) bannerMap.get("battr");
+                bannerWList = (List<String>) bannerMap.get("w");
+            }
+
+            List<String> instlList = (List<String>) element.get("instl");
+
+            ImpObject impObject = new ImpObject(videoLinkedHashMap, placementList, placementTypeList, enviromentTypeList, bannerHList, bannerBattrList, bannerWList, instlList);
 
 
+            if (impObjectList == null) {
+                impObjectList = new ArrayList<>();
+            }
 
-
-            //
+            impObjectList.add(impObject);
 
         }
-        System.out.println("asd");
-    }*/
+    }
+
+    public String getBidTime() {
+        return bidTime;
+    }
+
+    public List<String> getBundleList() {
+        return bundleList;
+    }
+
+    public List<String> getCatList() {
+        return catList;
+    }
+
+    public List<String> getCoppaList() {
+        return coppaList;
+    }
+
+    public List<String> getUserRatingList() {
+        return userRatingList;
+    }
+
+    public List<String> getLanguageList() {
+        return languageList;
+    }
+
+    public List<String> getDomainList() {
+        return domainList;
+    }
+
+    public List<String> getIdList() {
+        return idList;
+    }
+
+    public List<ImpObject> getImpObjectList() {
+        return impObjectList;
+    }
+
+    public List<String> getSegmentList() {
+        return segmentList;
+    }
+
+    public List<String> getTargetingCountry() {
+        return targetingCountry;
+    }
+
+    public List<String> getTargetingRegion() {
+        return targetingRegion;
+    }
+
+    public List<String> getTargetingMetro() {
+        return targetingMetro;
+    }
+
+    public List<String> getTargetingCity() {
+        return targetingCity;
+    }
+
+    public List<String> getInventorySourceList() {
+        return inventorySourceList;
+    }
+
+    public List<String> getTimeOfWeekList() {
+        return timeOfWeekList;
+    }
+
+    public List<String> getAugomentedOsList() {
+        return augomentedOsList;
+    }
+
+    public List<String> getAugmentedModelList() {
+        return augmentedModelList;
+    }
+
+    public List<String> getAugmentedBrowserList() {
+        return augmentedBrowserList;
+    }
+
+    public List<String> getAugmentedBrowserVersionList() {
+        return augmentedBrowserVersionList;
+    }
+
+    public List<String> getAugmentedOsvList() {
+        return augmentedOsvList;
+    }
+
+    public List<String> getAugmentedMakeList() {
+        return augmentedMakeList;
+    }
+
+    public List<String> getAugmentedDeviceScreenSizeList() {
+        return augmentedDeviceScreenSizeList;
+    }
+
+    public List<String> getCarrierList() {
+        return carrierList;
+    }
+
+    public List<String> getDeviceTypeList() {
+        return deviceTypeList;
+    }
+
+    public List<String> getConnectionTypeList() {
+        return connectionTypeList;
+    }
+
+    public List<String> getJsList() {
+        return jsList;
+    }
+
+    public class ImpObject {
+
+        private Map<String,Object> videoMap;
+        private List<String> placementList;
+        private List<String> placementTypeList;
+        private List<String> enviromentTypeList;
+        private List<String> bannerHList;
+        private List<String> bannerBattrList;
+        private List<String> bannerWList;
+        private List<String> instlList;
 
 
+        private ImpObject(Map<String, Object> videoMap,
+                          List<String> placementList,
+                          List<String> placementTypeList,
+                          List<String> enviromentTypeList,
+                          List<String> bannerHList,
+                          List<String> bannerBattrList,
+                          List<String> bannerWList,
+                          List<String> instlList) {
 
+            this.videoMap = videoMap;
+            this.placementList = placementList;
+            this.placementTypeList = placementTypeList;
+            this.enviromentTypeList = enviromentTypeList;
+            this.bannerHList = bannerHList;
+            this.bannerBattrList = bannerBattrList;
+            this.bannerWList = bannerWList;
+            this.instlList = instlList;
+        }
+
+        public Map<String, Object> getVideoMap() {
+            return videoMap;
+        }
+
+        public List<String> getPlacementList() {
+            return placementList;
+        }
+
+        public List<String> getPlacementTypeList() {
+            return placementTypeList;
+        }
+
+        public List<String> getEnviromentTypeList() {
+            return enviromentTypeList;
+        }
+
+        public List<String> getBannerHList() {
+            return bannerHList;
+        }
+
+        public List<String> getBannerBattrList() {
+            return bannerBattrList;
+        }
+
+        public List<String> getBannerWList() {
+            return bannerWList;
+        }
+
+        public List<String> getInstlList() {
+            return instlList;
+        }
+    }
 
 }
